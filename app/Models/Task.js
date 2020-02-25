b68fe2bde2454a06bf98128d163f150f9d693cf5
kelvynsantana <kelvyn.santana@icloud.com>
2020-02-25 18:11:50 -0300

    Add Tasks Hook

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -4,12 +4,19 @@
 const Model = use('Model')
 
 class Task extends Model {
+  static boot () {
+    super.boot()
+
+    this.addHook('afterCreate', 'TaskHook.sendNewTaskMail')
+    this.addHook('beforeUpdate', 'TaskHook.sendNewTaskMail')
+  }
+
   project () {
     return this.belongsTo('App/Models/Project')
   }
 
   user () {
-    return this.belongsTo('App/Models/Project')
+    return this.belongsTo('App/Models/User')
   }
 
   file () {
